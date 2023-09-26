7214e710968136ba0100707d87be1b64f8f9930e
Samhandal1 <samantha.handal@mail.mcgill.ca>
2023-09-25 21:12:05 -0400

    logic for sending/receiving from socket

--- dnsClient.py
+++ dnsClient.py
@@ -1,17 +1,55 @@
 import argparse
 import sys
+import time
+import socket
+
+BUFFER = 1024
+
+def build_packet(server_name, query_type):
+    packet = ""
+    return packet
 
 def send_dns_query(server, name, query_type, timeout, max_retries, port):
-    print(f"DnsClient sending request for {name}")
-    print(f"Server: {server}")
-    print(f"Request type: {query_type}")
 
-    # This is just a placeholder. Actual querying logic should be implemented here.
-    # This code will always return "NOTFOUND" for now.
-    
-    return "NOTFOUND"
+    # print summarized query
+    print("DnsClient sending request for " + name)
+    print("Server: " + server)
+    print("Request type: " + query_type)
+
+    # create client socket, set params
+    # A pair (host, port) is used for the AF_INET address family, where host is a string representing either a hostname in internet domain notation like 'daring.cwi.nl' or an IPv4 address like '100.50.200.5', and port is an integer.
+    # UDP (SOCK_DGRAM) is a datagram-based protocol. You send one datagram and get one reply and then the connection terminates.
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clientSocket.settimeout(timeout)
+    timer = time.perf_counter()
+
+    for i in range(max_retries + 1):
+        try:
+            # send
+            packet = build_packet(name, query_type)
+            clientSocket.sendto(packet, (server, port))
+
+            # receive
+            response, response_address = clientSocket.recvfrom(BUFFER)
+            curr_time = time.perf_counter() - timer
+            print("Response received after " + str(curr_time) + " seconds (" + str(i) + " retries)")
+            clientSocket.close()
+            return response
+
+        except socket.timeout:
+            if (i < max_retries):
+                print("ERROR    Socket timeout, unanswered query")
+            else:
+                print("ERROR    Maximum number of retries " + str(max_retries) + " exceeded")
+                clientSocket.close()
+                sys.exit(1)
+
+def parse_dns_response(result):
+    print("NOTFOUND")
 
 def main():
+
+    # parse arguments
     parser = argparse.ArgumentParser(description="DnsClient")
 
     parser.add_argument("-t", "--timeout", type=int, default=5, help="Timeout for retransmitting an unanswered query.")
@@ -24,6 +62,7 @@ def main():
 
     args = parser.parse_args()
 
+    # throw error if both flags given
     if args.mail_server and args.name_server:
         print("ERROR\tBoth -mx and -ns cannot be given simultaneously.")
         sys.exit(1)
@@ -34,8 +73,9 @@ def main():
     elif args.name_server:
         query_type = "NS"
 
+    # send logic
     result = send_dns_query(args.server, args.name, query_type, args.timeout, args.max_retries, args.port)
-    print(result)
+    parse_dns_response(result)
 
 if __name__ == "__main__":
     main()
